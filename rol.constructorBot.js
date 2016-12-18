bf088e1a344f992a2f5f18f131e5d84a7d863493
GCassay <geacassay@gmail.com>
2016-12-17 23:54:49 -0500

    Update rol.constructorBot.js

--- rol.constructorBot.js
+++ rol.constructorBot.js
@@ -1,42 +1,31 @@
 var rolConstructorBot = {
 
     run: function(creep, numConstructores) {
+	    creep.say("C BOT");
         
-        var contenedorX = parseInt(Memory.datos.contenedorX);
-        var contenedorY = parseInt(Memory.datos.contenedorY);
-        var extensionX = parseInt(Memory.datos.extensionX);
-        var extensionY = parseInt(Memory.datos.extensionY);
+        var contenedor2X = parseInt(Memory.datos.contenedor2X);
+        var contenedor2Y = parseInt(Memory.datos.contenedor2Y);
 
         if(creep.memory.construir && creep.carry.energy == 0) { // Creep en Modo Construcción / Sin energía
             creep.memory.construir = false; // Pasar a Modo Recolección para obtener más energía
-            creep.say('Recolectar');
+            //creep.say('Recolectar');
         }
         if(!creep.memory.construir && creep.carry.energy == creep.carryCapacity) { // Creep en Modo Recolección / Full energía
             creep.memory.construir = true; // Pasar a Modo Construcción para comenzar a construir
-            creep.say('Construir');
+            //creep.say('Construir');
         }
         if(creep.memory.construir) { // Creep en Modo Construcción / Con energía
         
-            // Empezar a constuir la Extensión
-            var punto = Game.rooms.sim.getPositionAt(extensionX,extensionY);
-            var extension = punto.findClosestByRange(FIND_CONSTRUCTION_SITES);
-            if(extension) {
+            var punto = Game.rooms.sim.getPositionAt(contenedor2X,contenedor2Y);
+            var contenedor = punto.findClosestByRange(FIND_CONSTRUCTION_SITES);
+            if(contenedor) {
                 if(creep.build(contenedor) == ERR_NOT_IN_RANGE){ // Desplazarse hasta el punto si no está en el rango
                     creep.moveTo(contenedor);
                 }
             }
-            else{ // Si la Extensión ya está construida, trabajar en el Contenedor
-                var punto = Game.rooms.sim.getPositionAt(contenedorX,contenedorY);
-                var contenedor = punto.findClosestByRange(FIND_CONSTRUCTION_SITES);
-                if(contenedor) {
-                    if(creep.build(contenedor) == ERR_NOT_IN_RANGE){ // Desplazarse hasta el punto si no está en el rango
-                        creep.moveTo(contenedor);
-                    }
-                }
-            }
         }
         else { // Modo Recolección 
-            var punto = Game.rooms.sim.getPositionAt(35,2);
+            var punto = Game.rooms.sim.getPositionAt(43,44);
             var fuente = punto.findClosestByRange(FIND_SOURCES_ACTIVE);
             // Si no está en el rango de la fuente, desplazarse hasta ella
             if(creep.harvest(fuente) == ERR_NOT_IN_RANGE) {
